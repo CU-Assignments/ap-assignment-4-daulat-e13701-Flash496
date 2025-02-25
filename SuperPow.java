bcf221758efb263a69cb5adf20b31ad56ba8b358
Debangshu Baul <121332582+Flash496@users.noreply.github.com>
2025-02-25 20:55:48 +0530

    Update SuperPow.java

--- SuperPow.java
+++ SuperPow.java
@@ -1,1 +1,25 @@
+class Solution {
+    private static final int MOD = 1337;
+
+    public int superPow(int a, int[] b) {
+        a %= MOD;
+        int result = 1;
+        
+        for (int digit : b) {
+            result = powerMod(result, 10) * powerMod(a, digit) % MOD;
+        }
+        return result;
+    }
+
+    private int powerMod(int x, int n) {
+        int res = 1;
+        x %= MOD;
+        while (n > 0) {
+            if (n % 2 == 1) res = res * x % MOD;
+            x = x * x % MOD;
+            n /= 2;
+        }
+        return res;
+    }
+}
 
