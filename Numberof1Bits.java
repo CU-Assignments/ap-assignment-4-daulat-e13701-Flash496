7bf318c32772a8ca1904af9926045d8d1450c5c5
Debangshu Baul <121332582+Flash496@users.noreply.github.com>
2025-02-25 14:29:42 +0530

    Update Numberof1Bits.java

--- Numberof1Bits.java
+++ Numberof1Bits.java
@@ -1,11 +1,10 @@
-class Solution {
-public:
-    int hammingWeight(uint32_t n) {
+public class Solution {
+    public int hammingWeight(int n) {
         int count = 0;
-        while (n) {
-            count += n & 1;
-            n >>= 1;
+        while (n != 0) {
+            count += n & 1; 
+            n >>>= 1; 
         }
         return count;
     }
-};
+}
