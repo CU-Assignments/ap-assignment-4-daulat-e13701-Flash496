1822fc0bb939da1866ee3f4e755e6b139fe6a735
Debangshu Baul <121332582+Flash496@users.noreply.github.com>
2025-03-02 23:25:45 +0530

    Create searchforarange.java

---
+++ searchforarange.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int n = nums.length;
+        int[] result = {-1, -1};
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == target) {
+                result[0] = i;
+                break;
+            }
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (nums[i] == target) {
+                result[1] = i;
+                break;
+            }
+        }
+
+        return result;
+    }
+}
